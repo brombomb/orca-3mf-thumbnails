217c9e5588ba2dd2d791e7eb856ce87645725e22
Rob Walsh <brombomb@gmail.com>
2024-01-12 13:42:26 -0700

    install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Install Requirements
+sudo apt install p7zip-full p7zip-rar python3
+
+script="3mf-thumbnailer"
+script_dest="/usr/bin/"
+helper="3mf.thumbnailer"
+helper_dest="/usr/share/thumbnailers/"
+
+move_file() {
+    sudo mkdir -p "$2"
+    sudo cp "$1" "$2"
+}
+
+move_file "$script" "$script_dest"
+move_file "$helper" "$helper_dest"
+
+# Set appropriate permissions
+sudo chmod +x "$script_dest/$script"
+
+# Display a message indicating successful installation
+echo -e "\e[32mThumbnailer installed successfully\e[0m"
