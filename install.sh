55ceef666bcc094dffa58e28ed55e8885dbe57e5
Rob Walsh <brombomb@gmail.com>
2024-01-12 20:38:12 -0700

    Fix install script

--- install.sh
+++ install.sh
@@ -4,20 +4,16 @@
 sudo apt install p7zip-full p7zip-rar python3
 
 script="3mf-thumbnailer.py"
-script_dest="/usr/bin/3mf-thumbnailer"
+script_dest="/usr/bin/"
+script_name="3mf-thumbnails"
 helper="3mf.thumbnailer"
 helper_dest="/usr/share/thumbnailers/"
 
-move_file() {
-    sudo mkdir -p "$2"
-    sudo cp "$1" "$2"
-}
+sudo cp "$script" "$script_dest/$script_name"
+sudo chmod +x "$script_dest/$script_name"
 
-move_file "$script" "$script_dest"
-move_file "$helper" "$helper_dest"
-
-# Set appropriate permissions
-sudo chmod +x "$script_dest"
+sudo mkdir -p "$helper_dest"
+sudo cp "$helper" "$helper_dest"
 
 # Display a message indicating successful installation
 echo -e "\e[32mThumbnailer installed successfully\e[0m"
